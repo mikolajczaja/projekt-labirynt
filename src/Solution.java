939a45bde1cefc7f3d948c82ed6f2ae3626d1bf4
mikolajczaja <pridak136@op.pl>
2017-04-10 21:24:34 +0200

    iterowanie licznikow przyległych pól, póżniej iterowanie będzie pojedynczych pól podczas 'przechodzenia" przez nie
    # Conflicts:
    #	bin/Solution.class

--- src/Solution.java
+++ src/Solution.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Solution {
 	
-	public static int SIZE=25;
+	public static int SIZE=5;
 	List<Field> stepList= new LinkedList<Field>();
 	Field[][] maze= new Field[SIZE][SIZE];
 
+	
 	void addList(Field field){
 		this.stepList.add(field);
 	}
@@ -48,12 +49,20 @@ public class Solution {
 	void printMaze(){
 		for(int i=0;i<SIZE;i++){
 			for(int j=0;j<SIZE;j++){
-				System.out.print(maze[j][i]);//+" | ");//:INFO odwrocone x i y
+				System.out.print(getField(i,j)); //:INFO odwrocone x i y
 			}
 		System.out.print("\n");
 		}
 	}
 	
+	void printMazeCounters(){
+		for(int i=0;i<SIZE;i++){
+			for(int j=0;j<SIZE;j++){
+				System.out.print(getField(i,j).getCounter()+"|");
+			}
+		System.out.print("\n");
+		}
+	}
 	
 	void setRandomWalls(){
 		
@@ -76,20 +85,35 @@ public class Solution {
 		
 		int positionX=field.getPositionX();
 		int positionY=field.getPositionY();
+		Field tmpField;
 		
-		Field tmpField=getField(positionX+1,positionY);
-		if(tmpField.getType()==0)tmpField.iterateCounter();
-		
-		tmpField=getField(positionX,positionY+1);
-		if(tmpField.getType()==0)tmpField.iterateCounter();
-		
-		tmpField=getField(positionX-1,positionY);
-		if(tmpField.getType()==0)tmpField.iterateCounter();
-		
-		tmpField=getField(positionX,positionY-1);
-		if(tmpField.getType()==0)tmpField.iterateCounter();
+		if(field.getType()==0){
+			if(positionX+1<SIZE){
+				tmpField=getField(positionX+1,positionY);
+				if(tmpField.getType()==0)field.iterateCounter();
+			}
+			if(positionY+1<SIZE){
+				tmpField=getField(positionX,positionY+1);
+				if(tmpField.getType()==0)field.iterateCounter();
+			}
+			if(positionX-1>0){
+				tmpField=getField(positionX-1,positionY);
+				if(tmpField.getType()==0)field.iterateCounter();
+			}
+			if(positionY-1>0){
+				tmpField=getField(positionX,positionY-1);
+				if(tmpField.getType()==0)field.iterateCounter();
+			}
+		}
 	}
 
+	void iterateAll(){
+		for(int i=0;i<SIZE;i++){
+			for(int j=0;j<SIZE;j++){
+				iterateNearbyCounters(getField(i,j));
+			}
+		}
+	}
 	
 	public static void main(String[] args){
 		Solution s1= new Solution();
@@ -100,9 +124,10 @@ public class Solution {
 		//s1.printList();
 		//s1.printNearbyFields(s1.getField(3,3));
 		//s1.setWall(4,3);
-		s1.setExit(24,6);
-		s1.iterateNearbyCounters(s1.getField(3,3));
+		s1.setExit(SIZE-1,SIZE-1);
+		s1.iterateAll();
 		s1.printMaze();	
+		s1.printMazeCounters();
 	}
 	
 }
